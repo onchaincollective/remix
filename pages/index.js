771f9c0b5bd4fe658055681a20812ab9d6aaa18d
yenargy <nitinramachandragupta@gmail.com>
2021-10-10 20:22:33 +0530

    Fixed build issue

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import Head from "next/head";
 import { useEffect, useState, useRef } from "react";
-import Web3, { utils } from "web3";
+import Web3 from "web3";
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 import { Web3ReactProvider, useWeb3React } from "@web3-react/core";
 import cn from "classnames";
-import Link from 'next/link'
 import { fabric } from "fabric";
 import { saveAs } from 'file-saver';
 
@@ -21,7 +20,6 @@ function getLibrary(provider) {
   return new Web3(provider);
 }
 
-let nftRenders;
 let canvas;
 
 export default function WrappedHome() {
@@ -37,32 +35,35 @@ function Home() {
     const [working, setWorking] = useState(false);
     const [loading, setLoading] = useState(false);
     const [bgImage, setBgImage] = useState(true);
-    const [nfts, setNfts] = useState(null);
+    const [nftWithoutFlwrs, setNftWithoutFlwrs] = useState(null);
+    const [nftFlwrs, setNftFlwrs] = useState(null);
     const [nftRenders, setNftRenders] = useState(null)
     
     useEffect(() => {
         if (!library) return;
+
         console.log("Fetching details for account: ", account)
+
         setWorking(true);
 
-        canvas = new fabric.Canvas("c");
-        fetch("https://api.opensea.io/api/v1/assets?owner=" + account +"&order_direction=desc&offset=0&limit=20")
+        fetch("https://api.opensea.io/api/v1/assets?owner=" + account +"&order_direction=desc&offset=0&limit=50")
         .then(res => res.json())
         .then(
           (result) => {
             console.log(result);
-            setNfts(result.assets);
+            let nftArray = result.assets;
+            let nftArrayFlwrs = nftArray.filter((nft) => {
+              return nft.asset_contract.address === contractAddress;
+            });
+            let nftArrayWithoutFlwrs = nftArray.filter((nft) => {
+              return nft.asset_contract.address !== contractAddress;
+            });
+            setNftWithoutFlwrs(nftArrayWithoutFlwrs);
+            setNftFlwrs(nftArrayFlwrs);
+            setNftWithoutFlwrs(result.assets);
             setWorking(false);
-            let nftRendersMap = result.assets.map((nft, i) =>  
-              <img 
-                crossOrigin="anonymous"
-                src={nft.image_url} 
-                key={i}
-                className="cursor-pointer w-full"
-                onClick={() => loadFile(nft.image_url, true)}
-                />
-            );
-            setNftRenders(nftRendersMap);
+            setNftImages(nftArrayWithoutFlwrs, true);
+            canvas = new fabric.Canvas("c");
           },
           (error) => {
             setWorking(false);
@@ -71,9 +72,17 @@ function Home() {
         
     }, [account]);
 
-    useEffect(() => {
-      console.log('bg image changin', bgImage);
-    }, [bgImage])
+    function setNftImages(nftArray, bgImage) {
+      let nftRendersMap = nftArray.map((nft, i) =>
+        <img 
+          src={nft.image_url} 
+          key={i}
+          className="cursor-pointer w-full rounded-lg"
+          onClick={() => loadFile(nft.image_url, bgImage)}
+          />
+      );
+      setNftRenders(nftRendersMap);
+    }
 
 
     function base64(str) {
@@ -89,82 +98,66 @@ function Home() {
     }
 
     function loadFile(url, bgImage) {
-      console.log("bgimg", bgImage);
       var output = document.getElementById("output");
       output.src = url;
-      output.crossOrigin = "Anonymous";
-      
-      getFileFromUrl(url, 'example.jpg').then((file) => {
-        var reader = new FileReader();
-        reader.readAsText(file, "UTF-8");
-        reader.onload = function (evt) {
-          let parser = new DOMParser();
-          let xmlDoc = parser.parseFromString(evt.target.result, "text/xml");
-          if (xmlDoc.getElementsByTagName("svg").length > 0) {
-            console.log('doin some svg manipulations');
-            xmlDoc.getElementsByTagName("svg")[0].setAttribute('width', 500);
-            xmlDoc.getElementsByTagName("svg")[0].setAttribute('height', 500);
-            
-            var y1 = xmlDoc.getElementsByTagName("rect")[0];
-            if (y1.getAttribute('filter') == "url(#filterBG)") {
-              let bg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 500 500" height="500" width="500">'
-                  + (new XMLSerializer()).serializeToString(xmlDoc.getElementById("filterBG"))
-                  + (new XMLSerializer()).serializeToString(y1) 
-                  + '</svg>';
+
+      if (bgImage) {
+        console.log('setting bgimg');
+        fabric.Image.fromURL(output.src, function(img) {
+          var img3 = img.set({left: 0,top: 0 })
+          img3.scaleToWidth(500);
+          canvas.setDimensions({width: 500, height: img3.getScaledHeight()});
+          canvas.setBackgroundImage(img3);
+          canvas.renderAll();
+        }, { crossOrigin: 'Anonymous' });
+      } else {
+        console.log('adding flowers');
+        getFileFromUrl(url, 'example.jpg').then((file) => {
+          var reader = new FileReader();
+          reader.readAsText(file, "UTF-8");
+          reader.onload = function (evt) {
+            let parser = new DOMParser();
+            let xmlDoc = parser.parseFromString(evt.target.result, "text/xml");
+            if (xmlDoc.getElementsByTagName("svg").length > 0) {
+              xmlDoc.getElementsByTagName("svg")[0].setAttribute('width', 500);
+              xmlDoc.getElementsByTagName("svg")[0].setAttribute('viewBox', "100 100 300 300");
               
-              bg = parser.parseFromString(bg, "text/xml");
-      
-              fabric.Image.fromURL("data:image/svg+xml;base64," + base64(bg), function(img) {
-                var img3 = img.set({left: 0,top: 0, lockMovementX: true, lockMovementY: true, selectable: false, opacity: 0.5, mode: 'overlay'  })
-                img3.scaleToWidth(500);
-                img3.crossOrigin="Anonymous";
-                canvas.add(img3);
-                canvas.renderAll();
-              }, { crossOrigin: 'Anonymous' });
+              var y1 = xmlDoc.getElementsByTagName("rect")[0];
+              if (y1.getAttribute('filter') == "url(#filterBG)") {
+                let bg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 500 500" height="500" width="500">'
+                    + (new XMLSerializer()).serializeToString(y1) 
+                    + '</svg>';
+                
+                bg = parser.parseFromString(bg, "text/xml");
+        
+                fabric.Image.fromURL("data:image/svg+xml;base64," + base64(bg), function(img) {
+                  var img3 = img.set({left: 0,top: 0, lockMovementX: true, lockMovementY: true, selectable: false, opacity: 0.5, mode: 'overlay'  })
+                  img3.scaleToWidth(500);
+                  canvas.add(img3);
+                  canvas.renderAll();
+                }, { crossOrigin: 'Anonymous' });
+              }
+              var y2 = xmlDoc.getElementsByTagName("rect")[1];
+              xmlDoc.documentElement.removeChild(y1);
+              xmlDoc.documentElement.removeChild(y2);
             }
-            var y2 = xmlDoc.getElementsByTagName("rect")[1];
-            xmlDoc.documentElement.removeChild(y1);
-            xmlDoc.documentElement.removeChild(y2);
-          }
-          
-          console.log("bgimg", bgImage);
-          if (!bgImage) {
-            console.log('setting svg');
+
             fabric.Image.fromURL("data:image/svg+xml;base64," + base64(xmlDoc), function(img) {
-              var img3 = img.set({left: 0,top: 0})
-              img3.scaleToWidth(500);
-              img3.crossOrigin="Anonymous";
+              var img3 = img.set({left: 100,top: 100})
+              img3.scaleToWidth(300);
               canvas.add(img3);
-              canvas.crossOrigin="anonymous";
               canvas.renderAll();
             },{ crossOrigin: 'Anonymous' });
-          }
 
-          if (bgImage) {
-            console.log('setting bgimg');
-            fabric.Image.fromURL(output.src, function(img) {
-              var img3 = img.set({left: 0,top: 0 })
-              img3.scaleToWidth(500);
-              img3.crossOrigin="Anonymous";
-              // console.log(img3.getScaledHeight());
-              canvas.setDimensions({width: 500, height: img3.getScaledHeight()});
-              canvas.setBackgroundImage(img3);
-              canvas.crossOrigin="anonymous";
-              canvas.renderAll();
-            }, { crossOrigin: 'Anonymous' });
-          }
-        };
-      }).catch(error => {
-        console.log("Error loading the file", error);
-      });
+          };
+        }).catch(error => {
+          console.log("There was an error loading the imgage :(", error);
+        });
+      }
     }
 
     function downloadPFP() {
-      document.getElementById("svg-tag").innerHtml = canvas.toSVG();
-      console.log(document.getElementById("svg-tag"));
-      console.log(canvas.getElement());
-      let image = canvas.toDataURL("image/png");
-      console.log(image);
+      canvas.discardActiveObject().renderAll();
       canvas.getElement().toBlob(function(blob) {
         saveAs(blob, "pfp.png");
       });
@@ -172,34 +165,8 @@ function Home() {
 
     function refreshNfts() {
       console.log('refreshing nfts');
-      setNfts(null);
       setNftRenders(null);
-      fetch("https://api.opensea.io/api/v1/assets?owner=" + account + "&asset_contract_address=0x5a876ffc6e75066f5ca870e20fca4754c1efe91f&order_direction=desc&offset=0&limit=20")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          console.log(result);
-          setNfts(result.assets);
-          setWorking(false);
-          let nftRendersMap = result.assets.map((nft, i) =>  
-            <img 
-              scrossOrigin="anonymous"
-              src={nft.image_url} 
-              key={i} 
-              className="cursor-pointer w-full"
-              onClick={() => loadFile(nft.image_url, false)}
-              />
-          );
-          setNftRenders(nftRendersMap);
-          setBgImage(false);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          setWorking(false);
-        }
-      )
+      setNftImages(nftFlwrs, false);
     }
     
 
@@ -228,43 +195,47 @@ function Home() {
         </Head>
       
         <div className="flex items-center flex-col max-w-2xl mx-auto text-center p-4">
-            <div className="mt-6 md:mt-10 flex flex-col items-center">
-                <header className="text-5xl md:text-6xl font-snell ml-8">
-                    flowers
-                </header>
+          <div className="mt-6 md:mt-10 flex flex-col items-center">
+              <header className="text-5xl md:text-6xl font-snell ml-8">
+                  flowers
+              </header>
 
-                <p className="text-center max-w-3xl mx-auto text-xl text-left mt-2 md:p-4 p-6">
-                  Connect your wallet to create pfp remixes from your NFTs
-                </p>
-            </div>
-            {!active && (
-                <div className="flex align-center flex-col max-w-4xl mx-auto text-xl text-left mt-8 pb-4">
-                    <ConnectButtons setWorking={setWorking} activate={activate} />
-                </div>
-            )}
-            {active && working && (
-                <div className="flex align-center flex-col max-w-4xl mx-auto text-xl text-left mt-8 pb-4">
-                    Loading your nfts
-                </div>
-            )}
-        </div>
-        <div className="flex flex-row space-x-8 max-w-5xl mx-auto text-center mt-10 mb-12">
-          <div className="">
-            <img id="output" crossOrigin="anonymous" className="hidden"/>
-            <canvas id="c" width="500" height="500" crossOrigin="anonymous"></canvas>
-            <div className="button cursor-pointer p-4 mt-8" hidden={bgImage} onClick={() => downloadPFP()}>Download</div>
-            <div id="svg-tag" crossOrigin="anonymous"></div>
+              <p className="text-center max-w-3xl mx-auto text-xl text-left mt-2 md:p-4 p-6">
+                Connect your wallet to create pfp remixes from your NFTs
+              </p>
           </div>
-          {active && !working && (
-            <div className="felx flex-col items-center justify-center">
-              <div className="grid grid-cols-4 gap-4 nfts"> 
-                {nftRenders}
+          {!active && (
+              <div className="flex align-center flex-col max-w-4xl mx-auto text-xl text-left mt-8 pb-4">
+                  <ConnectButtons setWorking={setWorking} activate={activate} />
               </div>
-              <div className="button cursor-pointer p-4 mt-8" onClick={() => refreshNfts()}>Next</div>
-            </div>
+          )}
+          {active && working && (
+              <div className="flex align-center flex-col max-w-4xl mx-auto text-xl text-left mt-8 pb-4">
+                  Loading your nfts
+              </div>
           )}
         </div>
-       
+        {active && !working && (
+          <div className="flex flex-col mx-auto text-center mt-10 mb-12 max-w-5xl">
+            <div className="flex flex-row space-x-8 ">
+              <div className="">
+                <img id="output" crossOrigin="anonymous" className="hidden"/>
+                <canvas id="c" width="500" height="500" crossOrigin="anonymous"></canvas>
+                <div className="button cursor-pointer p-4 mt-8" hidden={bgImage} onClick={() => downloadPFP()}>Download</div>
+                <div id="svg-tag" crossOrigin="anonymous"></div>
+              </div>
+              <div className="felx flex-col items-center justify-center">
+                <div className="grid grid-cols-4 gap-4 nfts"> 
+                  {nftRenders}
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-row justify-center space-x-8">
+              <div className="button cursor-pointer py-4 mt-8" onClick={() => downloadPFP()}>Download</div>
+              <div className="button cursor-pointer py-4 mt-8" onClick={() => refreshNfts()}>Next</div>
+            </div>
+          </div>
+        )}
         <div className="flex align-center flex-col max-w-2xl mx-auto text-center mt-10 mb-12 p-4">
             <div className="text-md ">
               <a href="https://twitter.com/OnChainCo" target="_blank" className="hover:underline">
